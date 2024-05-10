96049275def549a150209c43aca0b8db3e48a29b
Valid Code <thevalidcode@gmail.com>
2024-05-10 22:40:15 +0100

    Added creating of subdomain server

--- backend/utils/dns.js
+++ backend/utils/dns.js
@@ -19,33 +19,35 @@ function createServer(domain, res) {
     file "/var/lib/bind/${domain}.hosts";
   };
 `;
-
-    // Write updated content back to named.conf.local
-    fs.writeFile("/etc/bind/named.conf.local", updatedContent, (err) => {
-      if (err) {
-        console.error(`Error writing named.conf.local: ${err.message}`);
-        res.status(500).json({ error: "Internal server error" });
-        return;
-      }
-
-      // Execute BIND reload command
-      exec("systemctl reload bind9", (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error reloading BIND: ${error.message}`);
-          res.status(500).json({ error: "Internal server error" });
-          return;
-        }
-        if (stderr) {
-          console.error(`Error reloading BIND: ${stderr}`);
+    if (domain.includes(".validpanel.com")) {
+      createARecord(domain, "5.196.190.226");
+      res.json({ message: "Created successfully" });
+    } else {
+      // Write updated content back to named.conf.local
+      fs.writeFile("/etc/bind/named.conf.local", updatedContent, (err) => {
+        if (err) {
+          console.error(`Error writing named.conf.local: ${err.message}`);
           res.status(500).json({ error: "Internal server error" });
           return;
         }
 
-        // After successfully reloading BIND, create A record
-        createARecord(domain, "5.196.190.226");
-        res.json({ message: "Zone created successfully" });
+        // Execute BIND reload command
+        exec("systemctl reload bind9", (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error reloading BIND: ${error.message}`);
+            res.status(500).json({ error: "Internal server error" });
+            return;
+          }
+          if (stderr) {
+            console.error(`Error reloading BIND: ${stderr}`);
+            res.status(500).json({ error: "Internal server error" });
+            return;
+          }
+          createARecord(domain, "5.196.190.226");
+          res.json({ message: "Zone created successfully" });
+        });
       });
-    });
+    }
   });
 }
 
@@ -61,25 +63,57 @@ function createARecord(domain, ipAddress) {
 @ IN A ${ipAddress}
 www IN A ${ipAddress}
 `;
-
-  // Write zone file content to /var/lib/bind/{domain}.hosts
-  fs.writeFile(`/var/lib/bind/${domain}.hosts`, zoneFileContent, (err) => {
-    if (err) {
-      console.error(`Error writing zone file: ${err.message}`);
-      return;
-    }
-
-    // Execute BIND reload command
-    exec("systemctl reload bind9", (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error reloading BIND: ${error.message}`);
+  if (domain.includes(".validpanel.com")) {
+    fs.readFile("/var/lib/bind/validpanel.com.hosts", "utf8", (err, data) => {
+      if (err) {
+        console.error(`Error reading validpanel.com.hosts: ${err.message}`);
         return;
       }
-      if (stderr) {
-        console.error(`Stderr: ${stderr}`);
-        return;
-      }
+      const domainValue = domain.replace(/\.validpanel\.com$/, "");
+      const updatedContent =
+        data +
+        `
+    ${domainValue} IN A ${ipAddress}
+    www.${domainValue} IN A ${ipAddress}
+`;
+      fs.writeFile(
+        `/var/lib/bind/validpanel.com.hosts`,
+        updatedContent,
+        (err) => {
+          if (err) {
+            console.error(`Error writing zone file: ${err.message}`);
+            return;
+          }
+          exec("systemctl reload bind9", (error, stdout, stderr) => {
+            if (error) {
+              console.error(`Error reloading BIND: ${error.message}`);
+              return;
+            }
+            if (stderr) {
+              console.error(`Stderr: ${stderr}`);
+              return;
+            }
+          });
+        }
+      );
     });
-  });
+  } else {
+    fs.writeFile(`/var/lib/bind/${domain}.hosts`, zoneFileContent, (err) => {
+      if (err) {
+        console.error(`Error writing zone file: ${err.message}`);
+        return;
+      }
+      exec("systemctl reload bind9", (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error reloading BIND: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`Stderr: ${stderr}`);
+          return;
+        }
+      });
+    });
+  }
 }
 module.exports = { createServer };
